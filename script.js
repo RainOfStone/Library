3275c2e62cc724b49ccf9ef233a9f3aa07d90dee
RainOfStone <RainOfStone>
2025-02-11 22:52:58 -0500

    finished ui, made books be added to array

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ const BookCardBtn = document.getElementById('Read-Status')
 const BookCardTitle = document.getElementById('BookCardTitle')
 const BookCardAuthor = document.getElementById('BookCardAuthor')
 const BookCardPages = document.getElementById('BookCardPages')
+let count = 0
 function Book(title, author, pages, read) {
-    this.title = title //replace with in input val
-    this.author = author //replace with in input val
-    this.pages = pages //replace with in input val
-    this.read = read //replace with in input val
-    //form function goes here probably
+    this.title = title
+    this.author = author 
+    this.pages = pages 
+    this.read = read 
 }
 Book.prototype.BookItems = function() {
     console.log(this.title, this.author, this.pages, this.read)
@@ -38,22 +38,17 @@ dialog.addEventListener('click', (event) => {
                     ReadBtn.id = 'Read'
                 }
                 ReadBtn.value = 'Read'
-            //ReadStatus() changes status of whether it's read or not in the dialog
             break
             case 'submitBtn':
                 event.preventDefault()
                 console.log('event prevented!')
                 const book = new addBookToLibrary2(titleId.value, authorId.value, pagesId.value, ReadBtn.id, 'Btn')
-                book.AddBook()
+                console.log(myLibrary)
+                myLibrary.push(book.AddBook())
+                count++
                 dialog.close()
-                //prevent form from being submitted
-                //const Book = new Book(4X input values here)
-                //Book.Book()
-                //addBookToLibrary() make input vals for form = the objects ie. this.object
             break
         }
-        //addBookToLibrary()
-        //displayForm()
     })
 BookCard.addEventListener('click', (event) => {
     let target = event.target
@@ -70,6 +65,10 @@ BookCard.addEventListener('click', (event) => {
                 }
                 BookCardBtn.textContent = 'Read'
             break
+            case 'Delete':
+                BookCard.remove()
+            break
+
 }
 })
 headerButton.addEventListener('click', () => {
@@ -122,6 +121,24 @@ function addBookToLibrary2( title, author, pages, ReadStatus, DeleteBtn) {
 
         this.DeleteBtn.addEventListener('click', () => { //this is used to specify that instance of the object prevent all objects from being removed
             this.BookCardItem.remove()
+            myLibrary.splice(1, `${count}`)
+            console.log(myLibrary)
+        })
+        this.ReadStatus.addEventListener('click', () => {
+            if (this.ReadStatus.textContent == 'Not-Read') {
+                this.ReadStatus.textContent = 'Read'
+                this.ReadStatus.setAttribute('style', `height: 30px;
+            width: 100px;
+            border: none;
+            background-color: rgb(30, 92, 30);`)
+            }
+            else if (this.ReadStatus.textContent == 'Read') {
+                this.ReadStatus.textContent = 'Not-Read'
+                this.ReadStatus.setAttribute('style', `height: 30px;
+                    width: 100px;
+                    border: none;
+                    background-color: rgb(194, 23, 23);`)
+            }
         })
 
         libraryContainer.appendChild(this.BookCardItem)
@@ -133,185 +150,3 @@ function addBookToLibrary2( title, author, pages, ReadStatus, DeleteBtn) {
         
     }
 }
-/*function addBookToLibrary(title, author, pages, isread) {
-    const BookCard = document.createElement('div')
-    BookCard.setAttribute('style', `height: 100px;
-    width: clamp(300px, 800px, 900px);
-    background-color: white;
-    border: 1px solid black;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;`)
-    const BookCardBtn = document.createElement('button')
-    BookCardBtn.id = '#Read-Status'
-    BookCardBtn.textContent = 'Read'
-    BookCardBtn.setAttribute('style', `height: 30px;
-    width: 100px;
-    border: none;
-    background-color: rgb(30, 92, 30);`)
-    const BookCardTitle = document.createElement('div')
-    BookCardTitle.id = '#BookCardTitle'
-    const BookCardAuthor = document.createElement('div')
-    BookCardAuthor.id = '#BookCardAuthor'
-    const BookCardPages = document.createElement('div')
-    const BookCardDeleteBtn = document.createElement('button')
-    BookCardDeleteBtn.id = 'DeleteBtn'
-    BookCardDeleteBtn.textContent = 'X'
-    BookCardDeleteBtn.setAttribute('style', `height: 30px;
-        width: 30px;
-        border: none;
-        background-color: rgb(194, 23, 23);`)
-    BookCardPages.id = '#BookCardPages'
-    BookCardTitle.textContent = `Title: ${title}`
-    BookCardAuthor.textContent = `Author: ${author}`
-    BookCardPages.textContent = `Pages: ${pages}`
-    libraryContainer.appendChild(BookCard)
-    BookCard.appendChild(BookCardTitle)
-    BookCard.appendChild(BookCardAuthor)
-    BookCard.appendChild(BookCardPages)
-    BookCard.appendChild(BookCardBtn)
-    BookCard.appendChild(BookCardDeleteBtn)
-    const BookCardDeleteBtnSelector = document.querySelectorAll('#DeleteBtn')
-    BookCardDeleteBtnSelector.forEach(x => {
-        x.addEventListener('click', function() {
-            BookCard.remove()
-        })
-    })
-    
-    //takes params, creates book then stores in array
-    BookCardBtn.addEventListener('click', (event) => {
-        if (BookCardBtn.id == 'Read-Status') {
-            BookCardBtn.setAttribute('style', `height: 30px;
-                width: 100px;
-                border: none;
-                background-color: rgb(194, 23, 23)`)
-        }
-        if (BookCardBtn.id == 'Not-Read-Status') {
-            BookCardBtn.setAttribute('style', `height: 30px;
-                width: 100px;
-                border: none;
-                background-color: rgb(30, 92, 30)`)
-        }
-        let target = event.target
-        switch(target.id) {
-            case 'Read-Status':
-                if (BookCardBtn.id == 'Read-Status') {
-                    BookCardBtn.setAttribute('style', `height: 30px;
-                        width: 100px;
-                        border: none;
-                        background-color: rgb(194, 23, 23)`)
-                }
-            break
-            case 'Not-Read-Status':
-                if (BookCardBtn.id == 'Not-Read-Status') {
-                    BookCardBtn.id = 'Read-Status'
-                }
-                BookCardBtn.textContent = 'Read'
-            break
-
-            }
-    })}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Recreate this code by yourself
-/*function addBookToLibrary(bookCardItem, title, author, pages, BookStatusBtn, BookCardDeleteBtn) {
-    this.CreateBook = function() {
-    this.BookCardItem = document.createElement('div')
-    this.BookCardItem.setAttribute('style', `height: 100px;
-    width: clamp(300px, 800px, 900px);
-    background-color: white;
-    border: 1px solid black;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;`)
-    this.BookStatusBtn = document.createElement('div')
-    this.BookStatusBtn.id = 'Read-Status'
-    this.BookStatusBtn.textContent = BookStatusBtn
-    this.BookStatusBtn.setAttribute('style', `height: 30px;
-        width: 100px;
-        border: none;
-        background-color: rgb(30, 92, 30);
-        display: flex;
-        justify-content: center;
-        align-items: center`)
-    this.title = document.createElement('div')
-    this.title.id = 'BookCardTitle'
-    this.author = document.createElement('div')
-    this.author.id = 'BookCardAuthor'
-    this.pages = document.createElement('div')
-    this.pages.id = 'BookCardPages'
-    this.BookCardDeleteBtn = document.createElement('button')
-    this.BookCardDeleteBtn.id = 'DeleteBtn'
-    this.BookCardDeleteBtn.textContent = 'X'
-    this.BookCardDeleteBtn.setAttribute('style', `height: 30px;
-        width: 30px;
-        border: none;
-        background-color: rgb(194, 23, 23);`)
-    this.title.textContent = `Title: ${title}`
-    this.author.textContent = `Author: ${author}`
-    this.pages.textContent = `Pages: ${pages}`
-        this.BookCardDeleteBtn.addEventListener('click', () => {
-            this.BookCardItem.remove()
-        })
-        this.BookCardItem.appendChild(this.title)
-        this.BookCardItem.appendChild(this.author)
-        this.BookCardItem.appendChild(this.pages)
-        this.BookCardItem.appendChild(this.BookStatusBtn)
-        this.BookCardItem.appendChild(this.BookCardDeleteBtn)
-        libraryContainer.appendChild(this.BookCardItem)
-}}*/
-
-/*Use this to delete 1 item only:
-// Select all buttons with the class 'delete-button'
-        const buttons = document.querySelectorAll('.delete-button');
-
-        // Add click event listener to each button
-        buttons.forEach(button => {
-            button.addEventListener('click', function() {
-                // Remove the button that was clicked
-                this.remove();
-            });
-        });
-*/
